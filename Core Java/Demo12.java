6daaa2de4540712ce7236e7034c64634af6f6b09
Narendra8247 <158583515+Narendra8247@users.noreply.github.com>
2025-01-21 11:27:08 +0530

    Add files via upload

---
+++ Core Java/Demo12.java
@@ -0,0 +1,10 @@
+public class Demo12 {
+    public static void main(String[] args) {
+        String Name = "Narendra";
+        int Age = 20;
+        long PhNO = 1234567890;
+        while ((Name == "Narendra") && (Age == 20) && (PhNO == 1234567890)) {
+            break;
+        }
+    }
+}
